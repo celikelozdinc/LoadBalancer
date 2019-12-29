5fd037685f10dae182d2635c828520fc7ce98d8e
celikelozdinc <ozdinc.celikel@gmail.com>
2019-12-29 18:04:58 +0300

    loadbalancer only stores exchange, does not store queue/binding

--- src/main/java/tr/edu/itu/bbf/cloudcore/distributed/ipc/EventSender.java
+++ src/main/java/tr/edu/itu/bbf/cloudcore/distributed/ipc/EventSender.java
@@ -4,6 +4,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 
@@ -12,6 +13,9 @@ public class EventSender {
     @Autowired
     private RabbitTemplate rabbitTemplate;
 
+    @Value("${EVENT_EXCHANGE_SMOC2}")
+    private String EVENT_EXCHANGE_SMOC2;
+
     static final Logger logger = LoggerFactory.getLogger(EventSender.class);
 
     public EventSender(){}
@@ -19,9 +23,9 @@ public class EventSender {
     public void send(String host, String event)  {
         EventMessage msg = new EventMessage();
         msg.setEvent(event);
-        String exchange = "EVENT_EXCHANGE_" + host;
-        logger.info("Sending event {} to exchange {}",msg.getEvent(),exchange);
-        String reply = (String) rabbitTemplate.convertSendAndReceive(exchange,"rpc",msg);
+        //String exchange = "EVENT_EXCHANGE_" + host;
+        logger.info("Sending event {} to exchange {}",msg.getEvent(),EVENT_EXCHANGE_SMOC2);
+        String reply = (String) rabbitTemplate.convertSendAndReceive(EVENT_EXCHANGE_SMOC2,"rpc",msg);
         logger.info("Received reply from smoc = {}", reply);
     }
 
--- src/main/java/tr/edu/itu/bbf/cloudcore/distributed/ipc/RabbitConfig.java
+++ src/main/java/tr/edu/itu/bbf/cloudcore/distributed/ipc/RabbitConfig.java
@@ -10,27 +10,15 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class RabbitConfig {
-    @Value("${EVENT_QUEUE_SMOC2}")
-    private String EVENT_QUEUE_SMOC2;
 
     @Value("${EVENT_EXCHANGE_SMOC2}")
     private String EVENT_EXCHANGE_SMOC2;
 
-    /* Add queues and events for other smocs */
-
-    @Bean
-    Queue smoc2Queue() {
-        return new Queue(EVENT_QUEUE_SMOC2, false);
-    }
+    /* Add exchanges for other smocs */
 
     @Bean
     DirectExchange smoc2Exchange() {
         return new DirectExchange(EVENT_EXCHANGE_SMOC2);
-    }
-
-    @Bean
-    Binding binding(Queue smoc2Queue, DirectExchange smoc2Exchange) {
-        return BindingBuilder.bind(smoc2Queue).to(smoc2Exchange).with("rpc");
     }
 
 }
