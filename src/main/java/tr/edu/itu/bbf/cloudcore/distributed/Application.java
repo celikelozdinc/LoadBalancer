b95edd9cbcb27297f1de6930c6957e4d0768fcf9
celikelozdinc <ozdinc.celikel@gmail.com>
2020-01-02 20:07:22 +0300

    add zookeeper service inside compose

--- src/main/java/tr/edu/itu/bbf/cloudcore/distributed/Application.java
+++ src/main/java/tr/edu/itu/bbf/cloudcore/distributed/Application.java
@@ -26,22 +26,36 @@ public class Application implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-        sender.send(Hosts.SMOC1.toString(),Events.Pay.toString());
-        sender.send(Hosts.SMOC2.toString(),Events.Receive.toString());
-        sender.send(Hosts.SMOC3.toString(),Events.StartFromScratch.toString());
-        sender.send(Hosts.SMOC1.toString(),Events.Pay.toString());
-        sender.send(Hosts.SMOC2.toString(),Events.Receive.toString());
-        sender.send(Hosts.SMOC3.toString(),Events.StartFromScratch.toString());
-        sender.send(Hosts.SMOC1.toString(),Events.Pay.toString());
-        sender.send(Hosts.SMOC2.toString(),Events.Receive.toString());
-        sender.send(Hosts.SMOC3.toString(),Events.StartFromScratch.toString());
-        // iterate over enums using for loop
         /*
-        for (Events event : Events.values()) {
-             Hosts host = Hosts.values()[new Random().nextInt(Hosts.values().length)];
-             sender.send(host.toString(),event.toString());
-        }
+        sender.send(Hosts.SMOC1.toString(),Events.Pay.toString());
+        sender.send(Hosts.SMOC2.toString(),Events.Receive.toString());
+        sender.send(Hosts.SMOC3.toString(),Events.StartFromScratch.toString());
+        sender.send(Hosts.SMOC1.toString(),Events.Pay.toString());
+        sender.send(Hosts.SMOC2.toString(),Events.Receive.toString());
+        sender.send(Hosts.SMOC3.toString(),Events.StartFromScratch.toString());
+        sender.send(Hosts.SMOC1.toString(),Events.Pay.toString());
+        sender.send(Hosts.SMOC2.toString(),Events.Receive.toString());
+        sender.send(Hosts.SMOC3.toString(),Events.StartFromScratch.toString());
         */
+        // iterate over enums using for loop
+        for (Events event : Events.values()) {
+            Hosts host = Hosts.values()[new Random().nextInt(Hosts.values().length)];
+            logger.info("Sending event __{}__ to smoc __{}__",event.toString(),host.toString());
+            sender.send(host.toString(),event.toString());
+        }
+
+        for (Events event : Events.values()) {
+            Hosts host = Hosts.values()[new Random().nextInt(Hosts.values().length)];
+            logger.info("Sending event __{}__ to smoc __{}__",event.toString(),host.toString());
+            sender.send(host.toString(),event.toString());
+        }
+
+        for (Events event : Events.values()) {
+            Hosts host = Hosts.values()[new Random().nextInt(Hosts.values().length)];
+            logger.info("Sending event __{}__ to smoc __{}__",event.toString(),host.toString());
+            sender.send(host.toString(),event.toString());
+        }
+
     }
 
     public static void main(String[] args) {
