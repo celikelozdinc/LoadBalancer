16231d41db684751bcb39545f537a15aba23051c
celikelozdinc <ozdinc.celikel@gmail.com>
2020-01-30 14:22:07 +0300

    #smocs=11 for #events=9600

--- src/main/java/tr/edu/itu/bbf/cloudcore/distributed/ipc/RabbitConfig.java
+++ src/main/java/tr/edu/itu/bbf/cloudcore/distributed/ipc/RabbitConfig.java
@@ -29,7 +29,6 @@ public class RabbitConfig {
     @Value("${EVENT_EXCHANGE_SMOC7}")
     private String EVENT_EXCHANGE_SMOC7;
 
-    /*
     @Value("${EVENT_EXCHANGE_SMOC8}")
     private String EVENT_EXCHANGE_SMOC8;
 
@@ -42,6 +41,7 @@ public class RabbitConfig {
     @Value("${EVENT_EXCHANGE_SMOC11}")
     private String EVENT_EXCHANGE_SMOC11;
 
+    /*
     @Value("${EVENT_EXCHANGE_SMOC12}")
     private String EVENT_EXCHANGE_SMOC12;
 
@@ -90,7 +90,6 @@ public class RabbitConfig {
         return new DirectExchange(EVENT_EXCHANGE_SMOC7);
     }
 
-    /*
     @Bean
     DirectExchange smoc8Exchange() {
         return new DirectExchange(EVENT_EXCHANGE_SMOC8);
@@ -111,6 +110,7 @@ public class RabbitConfig {
         return new DirectExchange(EVENT_EXCHANGE_SMOC11);
     }
 
+    /*
     @Bean
     DirectExchange smoc12Exchange() {
         return new DirectExchange(EVENT_EXCHANGE_SMOC12);
