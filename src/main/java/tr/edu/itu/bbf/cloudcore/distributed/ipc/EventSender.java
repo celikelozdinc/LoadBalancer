fe57668f5e419d0e14574e81152f6f7fb4c2ec6d
celikelozdinc <ozdinc.celikel@gmail.com>
2020-01-31 10:51:29 +0300

    #smocs=7 for #events=4800

--- src/main/java/tr/edu/itu/bbf/cloudcore/distributed/ipc/EventSender.java
+++ src/main/java/tr/edu/itu/bbf/cloudcore/distributed/ipc/EventSender.java
@@ -26,7 +26,6 @@ public class EventSender {
     @Value("${EVENT_EXCHANGE_SMOC3}")
     private String EVENT_EXCHANGE_SMOC3;
 
-/*
     @Value("${EVENT_EXCHANGE_SMOC4}")
     private String EVENT_EXCHANGE_SMOC4;
 
@@ -39,6 +38,7 @@ public class EventSender {
     @Value("${EVENT_EXCHANGE_SMOC7}")
     private String EVENT_EXCHANGE_SMOC7;
 
+    /*
     @Value("${EVENT_EXCHANGE_SMOC8}")
     private String EVENT_EXCHANGE_SMOC8;
 
@@ -61,7 +61,8 @@ public class EventSender {
     private String EVENT_EXCHANGE_SMOC14;
 
     @Value("${EVENT_EXCHANGE_SMOC15}")
-    private String EVENT_EXCHANGE_SMOC15;*/
+    private String EVENT_EXCHANGE_SMOC15;
+    */
 
     private Dictionary exchangeDictionary;
 
@@ -79,10 +80,11 @@ public class EventSender {
         exchangeDictionary.put("SMOC1",EVENT_EXCHANGE_SMOC1);
         exchangeDictionary.put("SMOC2",EVENT_EXCHANGE_SMOC2);
         exchangeDictionary.put("SMOC3",EVENT_EXCHANGE_SMOC3);
-/*        exchangeDictionary.put("SMOC4",EVENT_EXCHANGE_SMOC4);
+        exchangeDictionary.put("SMOC4",EVENT_EXCHANGE_SMOC4);
         exchangeDictionary.put("SMOC5",EVENT_EXCHANGE_SMOC5);
         exchangeDictionary.put("SMOC6",EVENT_EXCHANGE_SMOC6);
         exchangeDictionary.put("SMOC7",EVENT_EXCHANGE_SMOC7);
+        /*
         exchangeDictionary.put("SMOC8",EVENT_EXCHANGE_SMOC8);
         exchangeDictionary.put("SMOC9",EVENT_EXCHANGE_SMOC9);
         exchangeDictionary.put("SMOC10",EVENT_EXCHANGE_SMOC10);
--- src/main/java/tr/edu/itu/bbf/cloudcore/distributed/ipc/RabbitConfig.java
+++ src/main/java/tr/edu/itu/bbf/cloudcore/distributed/ipc/RabbitConfig.java
@@ -17,7 +17,7 @@ public class RabbitConfig {
     @Value("${EVENT_EXCHANGE_SMOC3}")
     private String EVENT_EXCHANGE_SMOC3;
 
-/*    @Value("${EVENT_EXCHANGE_SMOC4}")
+    @Value("${EVENT_EXCHANGE_SMOC4}")
     private String EVENT_EXCHANGE_SMOC4;
 
     @Value("${EVENT_EXCHANGE_SMOC5}")
@@ -29,6 +29,7 @@ public class RabbitConfig {
     @Value("${EVENT_EXCHANGE_SMOC7}")
     private String EVENT_EXCHANGE_SMOC7;
 
+    /*
     @Value("${EVENT_EXCHANGE_SMOC8}")
     private String EVENT_EXCHANGE_SMOC8;
 
@@ -67,7 +68,7 @@ public class RabbitConfig {
     DirectExchange smoc3Exchange() {
         return new DirectExchange(EVENT_EXCHANGE_SMOC3);
     }
-/*
+
     @Bean
     DirectExchange smoc4Exchange() {
         return new DirectExchange(EVENT_EXCHANGE_SMOC4);
@@ -87,7 +88,7 @@ public class RabbitConfig {
     DirectExchange smoc7Exchange() {
         return new DirectExchange(EVENT_EXCHANGE_SMOC7);
     }
-
+/*
     @Bean
     DirectExchange smoc8Exchange() {
         return new DirectExchange(EVENT_EXCHANGE_SMOC8);
