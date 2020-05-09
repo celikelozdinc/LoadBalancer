fd0bc5509c9242f5f795f987d7382f969be66770
celikelozdinc <ozdinc.celikel@gmail.com>
2020-05-09 17:17:57 +0300

    centralized CKPTing

--- src/main/java/tr/edu/itu/bbf/cloudcore/distributed/Application.java
+++ src/main/java/tr/edu/itu/bbf/cloudcore/distributed/Application.java
@@ -14,7 +14,6 @@ import tr.edu.itu.bbf.cloudcore.distributed.service.InMemoryStore;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
-import java.util.stream.Collectors;
 
 
 @ComponentScan(basePackages = {"tr.edu.itu.bbf.cloudcore.distributed"})
@@ -62,13 +61,12 @@ public class Application implements CommandLineRunner {
                     logger.info("Working Mode = Randomized");
                     while(event < numberOfEvents) {
                         Hosts randomHost = Hosts.values()[new Random().nextInt(numberOfReplicas)];
-                        logger.info("...Starting cycle {} for hostname {}...", event, randomHost.toString());
-                        sendEventToEnsemble(randomHost.toString(), numberOfReplicas);
-                        logger.info("...Finished cycle {} for hostname {}...", event, randomHost.toString());
+                        logger.info("...Starting processing event {} inside host {}...", event, randomHost.toString());
+                        sendEventToEnsemble(randomHost.toString(), numberOfReplicas, false);
+                        logger.info("...Finished processing event {} inside host {}...", event, randomHost.toString());
                         event = event + 1;
                     }
                     break;
-
                     case "ordered":
                     logger.info("Working Mode = Ordered");
                     /* Prepare hosts list */
@@ -77,21 +75,20 @@ public class Application implements CommandLineRunner {
                         String host = "SMOC"+hostIndex;
                         hostsList.add(host);
                     }
-
                     while(event < numberOfEvents) {
                         for (String host : hostsList) {
-                            logger.info("...Starting cycle {} for hostname {}...",event,host);
-                            sendEventToEnsemble(host.toString(),numberOfReplicas);
-                            logger.info("...Finished cycle {} for hostname {}...",event,host);
+                            logger.info("...Starting event {} inside host {}...",event,host);
+                            sendEventToEnsemble(host.toString(),numberOfReplicas, true);
+                            logger.info("...Finished event {} inside host {}...",event,host);
+                            event = event + 1;
                         }
-                        event = event + 1;
                     }
                     break;
             }
 
     }
 
-    public void sendEventToEnsemble(String host, Integer numOfReplicas){
+    public void sendEventToEnsemble(String host, Integer numOfReplicas, boolean withCkpt){
         Events event = Events.Pay;
         logger.info("Sending {}.event which is __{}__ to __{}__ with flag __{}__", this.eventNumber, event.toString(),host,true);
         /* Send this message to chosen smoc in order to take CKPT  */
@@ -102,12 +99,18 @@ public class Application implements CommandLineRunner {
         /* Be sure that all the state machines are in same state after each event processing */
         for (int hostCounter=0; hostCounter < numOfReplicas; hostCounter ++ ) {
             Hosts otherHost = Hosts.values()[hostCounter];
+            /* otherHost = smocx, host = smocx -> do not process same event again and again */
             if (otherHost.toString().equals(host)) {
                 logger.info("Skipping host: __{}__",otherHost);
             }
-            else{
-                logger.info("Sending {}.event which is __{}__ to __{}__ with flag __{}__", this.eventNumber, event.toString(),otherHost,false);
-                String msg = sender.send(this.eventNumber, otherHost.toString(), event.toString(),false);
+            /* if solution is ... AND if the otherHost is peer of host*/
+
+            /* */
+            else {
+                /* if workingMode = ordered     -> solutionType = conventional                  -> all smocs stores each CKPT */
+                /* if workingMode = randomized  -> solutionType = distributed || centralized    -> no smoc stores any CKPT */
+                logger.info("Sending {}.event which is __{}__ to __{}__ with flag __{}__", this.eventNumber, event.toString(),otherHost,withCkpt);
+                String _msg = sender.send(this.eventNumber, otherHost.toString(), event.toString(),withCkpt);
             }
         }
         /*Calculate new event number*/
